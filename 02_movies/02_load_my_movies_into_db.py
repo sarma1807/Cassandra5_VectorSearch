e28ae0a1c123cafcff0f7e6bb3a0452b1977c58b
Sarma Pydipally <sarma1807@users.noreply.github.com>
2024-07-29 04:50:15 -0400

    Add files via upload

---
+++ 02_movies/02_load_my_movies_into_db.py
@@ -0,0 +1,70 @@
+###############
+#!/usr/bin/env python
+
+# imports
+import pandas as pd
+
+### import cassandra driver libraries and other modules
+from cassandra.cluster import Cluster
+from cassandra.auth    import PlainTextAuthProvider
+from cassandra.query   import SimpleStatement
+from cassandra         import ConsistencyLevel
+
+### connection variables for on-prem Cassandra 5.0-rc1 or above
+### multiple CASS_CONTACT_POINTS can be specified using comma separated strings
+CASS_CONTACT_POINTS    = ["192.168.1.177"]
+CASS_PORT              = 9042
+CASS_USERNAME          = "thor"
+CASS_PASSWORD          = "oracle"
+CASS_KEYSPACE          = "movies"
+CASS_WRITE_CONSISTENCY = ConsistencyLevel.ONE
+
+
+try:
+  ### cassandra connection
+  CASS_AUTH_PROVIDER = PlainTextAuthProvider(username = CASS_USERNAME, password = CASS_PASSWORD)
+  cass_cluster = Cluster(contact_points = CASS_CONTACT_POINTS, port = CASS_PORT, auth_provider = CASS_AUTH_PROVIDER)
+  cass_session = cass_cluster.connect(CASS_KEYSPACE)
+
+  ### prepare cql statements
+  cql_insert = cass_session.prepare("INSERT INTO my_movies (id, imdb_id, title, release_year, plot_overview) VALUES (?, ?, ?, ?, ?)")
+  cql_insert.consistency_level = CASS_WRITE_CONSISTENCY
+
+  ### read input csv file
+  df = pd.read_csv('my_movies.csv')
+  ### replace NULL/NaN strings with empty string
+  df = df.fillna('')
+
+  ### process each row from input csv file
+  for i in df.index:
+    try:
+      imdb_id       = df.at[i, 'imdb_id']
+      title         = df.at[i, 'title']
+      plot_overview = df.at[i, 'plot_overview']
+
+      ### replace empty strings with 0 before type casting
+      id            = int( 0 if df.at[i, 'id'] == '' else df.at[i, 'id'] )
+      release_year  = int( 0 if df.at[i, 'release_year'] == '' else df.at[i, 'release_year'] )
+
+      ### save the row in db
+      cass_session.execute(cql_insert, (id, imdb_id, title, release_year, plot_overview))
+
+    except Exception as dbe:
+      ### something went wrong while saving data in db
+      print("most likely this row has bad data : id = " + str(id) + " : " + str(dbe))
+
+  ### close cassandra connection
+  cass_cluster.shutdown()
+  cass_session.shutdown()
+
+except Exception as e:
+  ### something went wrong
+  print("something went wrong.")
+  print("")
+  print(e)
+else:
+  ### all went well
+  print("movies data loaded into database.")
+  print("Done.")
+###############
+
--- 02_movies/README.md
+++ 02_movies/README.md
@@ -1,1 +1,88 @@
-readme
+## movies (text search)
+
+#### this project can be used to demonstrate the ANN Vector Search in Cassandra 5.0-rc1 and above
+
+`Vector Search` in Cassandra 5.x is based on JVector https://github.com/jbellis/jvector <br>
+
+utilizes Approximate Nearest Neighbor (ANN) that in most cases yields results almost as good as the exact match. <br>
+The scaling is superior to Exact Nearest Neighbor (KNN).
+
+---
+
+### my_movies.csv
+
+this is a partial dataset extracted from "the-movies-dataset" on kaggle. <br>
+https://www.kaggle.com/datasets/rounakbanik/the-movies-dataset <br> <br>
+
+original movies_metadata.csv file is the main Movies Metadata file, which contains information on 45,000 movies featured in the Full MovieLens dataset, contains around 24 columns. <br> <br>
+
+` my_movies.csv ` contains 5 columns and 43,687 rows. <br> <br>
+id,imdb_id,title,release_year,plot_overview <br>
+
+- removed explicit content
+- removed special characters
+
+---
+
+### 01_cassandra_tables.cql
+
+- create a new keyspace in Cassandra
+- create a table for movies data
+- my_movies table contains a column with ` vector ` datatype (new in Cassandra 5.0-rc1 and above)
+- create a Storage-attached Index (SAI) : required on vector column to perform vector search
+
+##### execute after git clone :
+
+```
+cd ./Cassandra5_VectorSearch/movies
+
+cqlsh --file 01_cassandra_tables.cql
+```
+---
+
+### 02_load_my_movies_into_db.py
+
+python code to read each row from ` my_movies.csv ` and load it into Cassandra table. <br> <br>
+
+NOTE : this code does NOT create vector embeddings.
+
+##### execute after git clone :
+
+```
+cd ./Cassandra5_VectorSearch/movies
+
+python 02_load_my_movies_into_db.py
+```
+
+---
+
+### 03_load_vectors_into_db.py
+
+python code to read each row from ` my_movies.csv ` and creates vector embedding for ` plot_overview ` column, then saves the vector embeddings into Cassandra table.
+
+##### execute after git clone :
+
+```
+cd ./Cassandra5_VectorSearch/movies
+
+python 03_load_vectors_into_db.py
+```
+
+---
+
+### 04_moviesWebBrowser.py
+
+python web application to expose Cassandra data and also perform vector search based on user input. <br> <br>
+
+##### execute after git clone :
+
+```
+cd ./Cassandra5_VectorSearch/movies
+
+python 04_moviesWebBrowser.py
+```
+
+` screenshots ` folder contains few example screens.
+
+---
+
