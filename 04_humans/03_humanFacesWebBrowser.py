088fc0d655ea5ea7019cc8068a5febddb6de020f
Sarma Pydipally <sarma1807@users.noreply.github.com>
2024-08-02 10:59:29 -0400

    Add files via upload

---
+++ 04_humans/03_humanFacesWebBrowser.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+# imports
+import random
+import tornado.web
+import tornado.ioloop
+import os
+from PIL import Image
+from sentence_transformers import SentenceTransformer
+
+### import cassandra driver libraries and other modules
+from cassandra.cluster import Cluster
+from cassandra.auth    import PlainTextAuthProvider
+from cassandra.query   import SimpleStatement
+from cassandra         import ConsistencyLevel
+
+### variables
+WEB_IMGS_FOLDER_ABS_PATH = "~"
+WEB_IMGS_FOLDER = "faceImgs"
+WEB_PORT = "8080"
+
+### connection variables for Cassandra 5.0-rc1 or above
+### multiple CASS_CONTACT_POINTS can be specified using comma separated strings
+CASS_CONTACT_POINTS    = ["192.168.1.177"]
+CASS_PORT              = 9042
+CASS_USERNAME          = "thor"
+CASS_PASSWORD          = "oracle"
+CASS_KEYSPACE          = "human"
+CASS_READ_CONSISTENCY  = ConsistencyLevel.ONE
+
+### transformer for vectors
+model = SentenceTransformer('clip-ViT-B-32')
+
+### auth for cassandra connection
+CASS_AUTH_PROVIDER = PlainTextAuthProvider(username = CASS_USERNAME, password = CASS_PASSWORD)
+
+
+class basicRequestHandler(tornado.web.RequestHandler):
+  def get(self):
+    vColumnCount = 0
+    self.write("<h2> <a href='./form'> Click here to run Vector Search </a> </h2> \n")
+    self.write("<h3> Basic Grid of Human Faces </h3> \n")
+    self.write("<table border=0> <tr> \n")
+    for vfile in random.sample(os.listdir(WEB_IMGS_FOLDER), 80):
+      # check for filename ending with .jpg and it is a file and it is not zero bytes in size
+      if vfile.endswith(".jpg") and os.path.isfile(WEB_IMGS_FOLDER + '/' + vfile) and os.path.getsize(WEB_IMGS_FOLDER + '/' + vfile) > 0:
+        if vColumnCount % 10 == 0:
+          self.write("</tr><tr> \n")
+        self.write("<td align=center valign=center> <img src=" + WEB_IMGS_FOLDER + '/' + vfile + " width='50%' height='50%'> </td> \n")
+        vColumnCount = vColumnCount + 1
+    self.write("</tr></table> \n")
+
+class formRequestHandler(tornado.web.RequestHandler):
+  def get(self):
+    vColumnCount = 0
+    self.write("<h3> Click on a Human Face Image to perform ANN similarity search ... </h3> <br> \n")
+    self.write("<head> <style> .image-button { display: inline-block; cursor: pointer; background-color: white; width: 100px; height: 100px; } </style> </head> \n")
+    self.write("<body> <form action='/formClicked' method='get'> \n")
+    for vfile in random.sample(os.listdir(WEB_IMGS_FOLDER), 20):
+      # check for filename ending with .jpg and it is a file and it is not zero bytes in size
+      if vfile.endswith(".jpg") and os.path.isfile(WEB_IMGS_FOLDER + '/' + vfile) and os.path.getsize(WEB_IMGS_FOLDER + '/' + vfile) > 0:
+        if vColumnCount % 10 == 0:
+          self.write("<br> \n")
+        vImgFile = WEB_IMGS_FOLDER + '/' + vfile
+        self.write("<button type='submit' name='imgFile' value='" + vfile + "'> <img src='" + vImgFile + "' class='image-button'> </button> \n")
+        vColumnCount = vColumnCount + 1
+    self.write("</form> </body>")
+
+class formClickedRequestHandler(tornado.web.RequestHandler):
+  def get(self):
+    ### capture get input
+    imgFile = self.get_argument("imgFile")
+    vImgFile = WEB_IMGS_FOLDER + '/' + imgFile
+    ### generate vector embedding and convert it to a list
+    vImgFile_vector = model.encode(Image.open(vImgFile)).tolist()
+
+    ### cassandra connection
+    cass_cluster = Cluster(contact_points = CASS_CONTACT_POINTS, port = CASS_PORT, auth_provider = CASS_AUTH_PROVIDER)
+    cass_session = cass_cluster.connect(CASS_KEYSPACE)
+    cql_cars_ann_search = cass_session.prepare("SELECT face_image FROM faces ORDER BY face_image_vector ANN OF ? LIMIT 10")
+
+    cql_stmt = cql_cars_ann_search.bind([vImgFile_vector])
+    cql_stmt.consistency_level = CASS_READ_CONSISTENCY
+    cass_output_1 = cass_session.execute(cql_stmt)
+    vColumnCount = 0
+
+    self.write("<table border=0> <tr> <td> <h3> input human face </h3> </td> <td> <img src='" + vImgFile + "' width='40%' height='40%'> <tr> </table> <br> <hr> \n")
+    self.write("<h3> 10 human faces similar to the input face (search results using Cassandra 5.x ANN Vector Search) </h3> <br> \n")
+    self.write("<table border=0> <tr> \n")
+
+    for cass_row in cass_output_1:
+      if vColumnCount % 5 == 0:
+        self.write("</tr> <tr> \n")
+      self.write("<td> <img src='" + WEB_IMGS_FOLDER + "/" + cass_row.face_image + "' width='40%' height='40%'> </td> \n")
+      vColumnCount = vColumnCount + 1
+    self.write("</tr> </table> \n")
+
+    ### close cassandra connection
+    cass_cluster.shutdown()
+    cass_session.shutdown()
+
+
+if __name__ == "__main__":
+  app = tornado.web.Application([
+    (r"/", basicRequestHandler),
+    (r"/form", formRequestHandler),
+    (r"/formClicked", formClickedRequestHandler),
+    (r"/" + WEB_IMGS_FOLDER + "/(.*)", tornado.web.StaticFileHandler, {"path": WEB_IMGS_FOLDER_ABS_PATH})
+  ])
+  
+  app.listen(WEB_PORT)
+  print(f"web application is ready and listening on port {WEB_PORT}")
+  tornado.ioloop.IOLoop.current().start()
+
--- 04_humans/README.md
+++ 04_humans/README.md
@@ -1,1 +1,109 @@
-readme
+## humans (image search)
+
+#### this project can be used to demonstrate the ANN Vector Search in Cassandra 5.0-rc1 and above
+
+`Vector Search` in Cassandra 5.x is based on JVector https://github.com/jbellis/jvector <br>
+
+utilizes Approximate Nearest Neighbor (ANN) that in most cases yields results almost as good as the exact match. <br>
+The scaling is superior to Exact Nearest Neighbor (KNN).
+
+---
+
+### human-faces dataset
+
+https://www.kaggle.com/datasets/ashwingupta3012/human-faces/
+
+this is a partial dataset extracted from "Humans.zip" from DVM-CAR. <br> <br>
+
+## NOTE : this is not a very clean dataset (it contains duplicates), but Vector Search results are pretty impressive
+
+` Cassandra5_VectorSearch / 04_humans / faceImgs ` folder contains 3 zip files. This compressed dataset contains 3,000 human face images.
+
+```
+0001_1000.zip
+1001_2000.zip
+2001_3000.zip
+```
+
+## prepare the dataset
+
+##### after git clone :
+
+```
+cd ./Cassandra5_VectorSearch/04_humans/faceImgs
+
+unzip -j 0001_1000.zip -d .
+unzip -j 1001_2000.zip -d .
+unzip -j 2001_3000.zip -d .
+```
+
+##### if everything goes well, then we should get 3,000 JPG files in ` Cassandra5_VectorSearch / 04_humans / faceImgs ` folder.
+
+```
+$ pwd
+/apps/opt/cassandra/Cassandra5_VectorSearch/04_humans/faceImgs
+
+$ ls -lh *.jpg | wc -l
+3000
+$
+```
+
+## we are now ready to use this dataset
+
+---
+
+### 01_cassandra_tables.cql
+
+- create a new keyspace in Cassandra
+- create a table for human faces data
+- faces table contains a column with ` vector ` datatype (new in Cassandra 5.0-rc1 and above)
+- create a Storage-attached Index (SAI) : required on vector column to perform vector search
+
+##### execute after git clone :
+
+```
+cd ./Cassandra5_VectorSearch/04_humans
+
+cqlsh --file 01_cassandra_tables.cql
+```
+---
+
+### 02_load_vectors_into_db.py
+
+python code to read each image from ` faceImgs ` folder and creates vector embedding for the image, then saves the vector embeddings into Cassandra table.
+
+##### execute after git clone :
+
+```
+cd ./Cassandra5_VectorSearch/04_humans
+
+python 02_load_vectors_into_db.py
+```
+
+---
+
+### 03_humanFacesWebBrowser
+
+python web application to expose Cassandra data and also perform vector search based on user input. <br> <br>
+
+##### execute after git clone :
+
+```
+cd ./Cassandra5_VectorSearch/04_humans/faceImgs
+
+sed -i "s|~|$PWD|g" ../03_humanFacesWebBrowser
+
+cd ..
+
+python 03_humanFacesWebBrowser
+```
+##### web application URL :
+
+```
+http://localhost:8080
+```
+
+` screenshots ` folder contains few example screens.
+
+---
+
